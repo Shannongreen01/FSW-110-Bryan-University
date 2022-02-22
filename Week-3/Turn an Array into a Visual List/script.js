04668061b84320ac2fff39425602981c1ebb00f8
Shannongreen01 <96604457+Shannongreen01@users.noreply.github.com>
2022-02-22 15:00:17 -0500

    Add files via upload

---
+++ Week-3/Turn an Array into a Visual List/script.js
@@ -0,0 +1,27 @@
+
+for ( let i = 0; i < 10; i ++) {
+const header = document.createElement("h1");
+header.innerHTML = "Hello World";
+header.style.color = 'red';
+document.body.appendChild(header);
+}
+
+
+const names = ["steve", "larry", "joe","shirley", "steph","nate" ,"rick", "emily"]
+
+for(i = 0; i < names.length; i++ ){
+var h2 = document.createElement("h2")
+h2.textContent = names[i]
+document.body.append(h2)
+}
+
+
+
+
+
+
+
+
+
+
+
