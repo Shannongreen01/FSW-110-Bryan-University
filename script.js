dd11ba11b5fecf3bb332bd5bab2c6ddf8873a009
Shannongreen01 <96604457+Shannongreen01@users.noreply.github.com>
2022-03-03 11:43:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var names = ["I", "Like", "Learning", "New", "Things"];
+
+var i;
+
+for (i = 0; i < names.length; i++) {
+    var h2 = document.createElement("h2")
+    
+    h2.textContent = names[i]
+    h2.style.fontSize = "20px";
+    h2.style.fontWeight = "lighter";
+    h2.style.fontFamily = "sans-serif"
+    h2.style.color = "cornflowerblue";
+    h2.classList.add("border");
+    document.body.append(h2)
+
+}
